58ea0f769d9ad5e745f113370751abe5ec275560
Bokka Balajiravikishore <balajiravikishore.b@incedoinc.com>
2020-06-04 20:26:15 +0530

    Updates

---
+++ precache-manifest.8f032f001e50ebcb4d69c62b3bd24354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6249e1d156aa0bb4c888e3ec76c11c13",
+    "url": "/baluravi999/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "a96ae9043d64ead1ccf4",
+    "url": "/baluravi999/monsters-rolodex-complete/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/baluravi999/monsters-rolodex-complete/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/baluravi999/monsters-rolodex-complete/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96ae9043d64ead1ccf4",
+    "url": "/baluravi999/monsters-rolodex-complete/static/js/main.db4f1744.chunk.js"
+  },
+  {
+    "revision": "3b1f5a51ae41b575089c",
+    "url": "/baluravi999/monsters-rolodex-complete/static/js/runtime-main.68178018.js"
+  }
+]);
